be5fd96d0964254e9bcbbcd01762d6774951b1ad
Rauf Erk <erk.rauf@gmail.com>
2025-12-05 14:56:54 +0300

    добавили стили триплекса

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,24 @@
 // src/main.tsx
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import App from "./App.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import App from './App.tsx'
 
-import "./styles/fonts.css";
-import "./styles/globals.css";
-import "./styles/index.css";
+import './styles/fonts.css'
+import './styles/globals.css'
+import './styles/index.css'
 
-import { store } from "./store";
-import { ThemeSync } from "./ThemeSync";
+import '@sberbusiness/triplex-next/styles/triplex-next.css'
+import '@sberbusiness/icons-next/styles/icons.css'
 
-createRoot(document.getElementById("root")!).render(
+import { store } from './store'
+import { ThemeSync } from './ThemeSync'
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <ThemeSync />
       <App />
     </Provider>
   </StrictMode>
-);
+)
